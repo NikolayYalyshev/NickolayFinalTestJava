6212d12aabdceeed7ec3fdcc54351f215b286feb
NikolayYalyshev <Nikolay.Yalyshev1@Gmail.com>
2023-09-10 12:41:01 +0300

    10.09.23 Update html, css. JS is not found! TODO JS

--- src/main/resources/static/registration/registrationAction.js
+++ src/main/resources/static/registration/registrationAction.js
@@ -1,87 +1,62 @@
-const form = {
-    name: document.getElementById('name'),
-    email: document.getElementById('email'),
-    password: document.getElementById('password'),
-    button: document.querySelector('button'),
-    error: document.querySelector('.input-error')
-}
+const form = document.getElementById('form');
+document.addEventListener("DOMContentLoaded", function() {
 
-function checkForm() {
-    const name = form.name.getElementsByTagName('input')[0].value
-    const email = form.email.getElementsByTagName('input')[0].value
-    const password = form.password.getElementsByTagName('input')[0].value
-    if(name && email && password) {
-        form.button.classList.remove('disable')
-    } else {
-        form.button.classList.add('disable')
+    const inputs = form.querySelectorAll('input[type="text"], input[type="email"], input[type="password"]');
+    const button = form.querySelector('.button');
+
+    function checkForm() {
+        const name = form.querySelector('input[name="name"]').value;
+        const email = form.querySelector('input[name="email"]').value;
+        const password = form.querySelector('input[name="password"]').value;
+
+        console.log(name, email, password);
+
+        if (name && email && password) {
+            button.classList.remove('disable');
+            button.disabled = false;
+        } else {
+            button.classList.add('disable');
+            button.disabled = true;
+        }
     }
-}
 
-function hundleInput(e, name) {
-    const { value } = e.target
-    if(value) {
-        form[name].classList.add('filed')
-    } else {
-        form[name].classList.remove('filed')
-    }
-    checkForm()
-}
-
-function validateEmail() {
-    const { value } = form.email.getElementsByTagName('input')[0]
-    const reg = /[a-z]{3,20}@[a-z]{3,10}\.[a-z]{2,4}/
-    console.log(value)
-    if(reg.test(value)){
-        form.email.classList.remove('error')
-        form.error.classList.remove('view')
-    } else {
-        form.button.classList.add('disable')
-        form.email.classList.add('error')
-        form.error.classList.add('view')
-    }
-}
-
-form.name.oninput = (e) => hundleInput(e, 'name')
-form.email.oninput = (e) => hundleInput(e, 'email')
-form.password.oninput = (e) => hundleInput(e, 'password')
-form.button.onclick = validateEmail
-
-const doc = document.getElementById('form')
-const usernameInput = document.getElementById('username')
-const emailInput = document.getElementById('user-email')
-const passwordInput = document.getElementById('user-password')
+    inputs.forEach(input => {
+        input.addEventListener('input', checkForm);
+    });
+});
 
 function showErrorMessage(massage){
-    const errorElement = document.createElement('span')
-    errorElement.classList.add('error-massage')
-    errorElement.textContent = massage
-    doc.appendChild(errorElement)
+    const errorElement = document.createElement('span');
+    errorElement.classList.add('error-massage');
+    errorElement.textContent = massage;
+    form.appendChild(errorElement);
 }
 
-doc.addEventListener('submit', async (event) => {
-    event.preventDefault()
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-       const name = usernameInput.value
-      const email = emailInput.value
-      const password = passwordInput.value
+    const username = form.querySelector('input[name="name"]').value;
+        const email = form.querySelector('input[name="email"]').value;
+        const password = form.querySelector('input[name="password"]').value;
 
-      console.log(name, email, password)
+      console.log(username, email, password);
 
     try{
-        const response = await fetch('/Person', {
+        const response = await fetch('/person', {
             method: 'POST',
-            body: JSON.stringify({name, email, password}),
+            body: JSON.stringify({username, email, password}),
             headers:{'Content-Type' : 'application/json'}
         })
         if(response.ok){
-            window.location.href = '/mainpage'
+            window.location.href = '/mainpage';
         } else {
-            const error = await response.json()
-            const errorMassage = error.massage || 'Произошла ошибка'
+            const error = await response.json();
+            const errorMassage = error.massage || 'Произошла ошибка';
             showErrorMessage(errorMassage)
         }
     } catch(error) {
         showErrorMessage("Произошла ошибка")
     }
 });
+
 
